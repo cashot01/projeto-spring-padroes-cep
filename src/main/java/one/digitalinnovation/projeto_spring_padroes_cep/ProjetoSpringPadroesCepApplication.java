1f761b55cd57429bd85f99717b6131db34db5c58
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2026-01-22 16:30:17 -0300

    chore: initial commit

---
+++ src/main/java/one/digitalinnovation/projeto_spring_padroes_cep/ProjetoSpringPadroesCepApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projeto_spring_padroes_cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringPadroesCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringPadroesCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/projeto_spring_padroes_cep/ProjetoSpringPadroesCepApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projeto_spring_padroes_cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringPadroesCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
